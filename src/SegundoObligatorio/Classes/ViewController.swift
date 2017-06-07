62ba52f9e2c73b35dc03eee98fdd5cf9850ffb1a
gonzalobarrios <gonbarrios1@hotmail.com>
2017-06-06 21:35:37 -0300

    latitude & longitud

--- src/SegundoObligatorio/Classes/ViewController.swift
+++ src/SegundoObligatorio/Classes/ViewController.swift
@@ -7,8 +7,13 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
+import CoreLocation
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
+    var latitude: Double = 0
+    var longitude: Double = 0
+    
 
     // Main board
     @IBOutlet weak var cityLabel: UILabel!
@@ -18,19 +23,48 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //For use when the app is open & in the background
+        locationManager.requestAlwaysAuthorization()
+        
+        //For use when the app is open
+        //locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled(){
+            locationManager.delegate=self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.requestWhenInUseAuthorization()
+            locationManager.startUpdatingLocation()
+            
+            
+            
+        }
+        
 //        condition: weather.id
 //        iconString: weather.icon
 //        ver: http://openweathermap.org/current#parameter
 //        http://openweathermap.org/weather-conditions
         
         self.weatherIconLabel.text = WeatherIcon(condition: 200, iconString: "01n").iconText
+        
+        
     }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            latitude = location.coordinate.latitude
+            longitude = location.coordinate.longitude
+            
+            print(latitude)
+            print(longitude)
+        }
+    }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
