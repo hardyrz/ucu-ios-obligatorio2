3c1bc0c6967d07bb6cdbe06a9409ed9676d34731
Brian Hardy <hardyr.93@gmail.com>
2017-06-09 19:56:14 -0300

    Cell implemented!!

--- src/SegundoObligatorio/Classes/ViewController.swift
+++ src/SegundoObligatorio/Classes/ViewController.swift
@@ -1,17 +1,19 @@
 import UIKit
 import CoreLocation
-class ViewController: UIViewController, CLLocationManagerDelegate {
+class ViewController: UIViewController, CLLocationManagerDelegate, UICollectionViewDelegate, UICollectionViewDataSource  {
     
     let locationManager = CLLocationManager()
     var latitude: Double = 0
     var longitude: Double = 0
-    
+
+    var weatherForecastList = [JSONWeatherForecastMappable]()
 
     // Main board
     @IBOutlet weak var cityLabel: UILabel!
     @IBOutlet weak var weatherIconLabel: UILabel!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var unitLabel: UILabel!
+    @IBOutlet weak var collectionView: UICollectionView!
     
     
     var unit = "imperial"
@@ -20,7 +22,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.collectionView.backgroundColor = UIColor .clear
         //For use when the app is open & in the background
         locationManager.requestAlwaysAuthorization()
         
@@ -37,8 +39,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             
         }
     }
-
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if let location = locations.first {
             self.lat = String(location.coordinate.latitude)
             self.lon = String(location.coordinate.longitude)
@@ -64,7 +65,9 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             
             
             ApiClientWeather.shared.getForecastWeather(unit, self.lat, self.lon){ (weatherList, error) in
-                
+                if let weatherList = weatherList {
+                    self.weatherForecastList = weatherList
+                }
                 /*for weather in weatherList as [JSONWeatherForecastMappable]! {
                     self.weatherIconDayLabel.text = WeatherIcon(condition: weather.iconId, iconString: weather.iconString).iconText
                     self.temperatureDayLabel.text = String(weather.temperature)
@@ -87,6 +90,31 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    // UICollectionViewDelegate y UICollectionViewDelegate
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+         return 10
+    }
 
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "dayCell", for: indexPath) as! CustomCollectionViewCell
+        
+        let weather = self.weatherForecastList[indexPath.row] // usar este weather!!!
+        cell.dayWeekLabel.text = "LUNES!"
+        cell.dayWeatherIconDayLabel.text = ""
+        cell.dayTemperatureLabel.text = String(25)
+        if (self.unit == "metric") {
+            cell.dayUnitLabel.text = "ºC"
+        } else {
+            cell.dayUnitLabel.text = "ºF"
+        }
+        
+        return cell
+    }
 }
+
+
+
+
 
---
+++ src/SegundoObligatorio/CustomCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomCollectionViewCell.swift
+//  SegundoObligatorio
+//
+//  Created by SP 23 on 9/6/17.
+//  Copyright © 2017 Universidad Catolica. All rights reserved.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var dayWeekLabel: UILabel!
+    @IBOutlet weak var dayWeatherIconDayLabel: UILabel!
+    @IBOutlet weak var dayTemperatureLabel: UILabel!
+    @IBOutlet weak var dayUnitLabel: UILabel!
+    
+}
